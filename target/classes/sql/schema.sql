972e80889b5f389cf619c4a7f268910168ba9c92
Johannes Joestar <thejohannesjoestar@gmail.com>
2020-05-10 18:28:48 +0300

    prettyfied sql

--- target/classes/sql/schema.sql
+++ target/classes/sql/schema.sql
@@ -1,1 +1,7 @@
-CREATE TABLE weatherdata(id IDENTITY PRIMARY KEY, city VARCHAR(255), date_measured DATE, temperature_min DOUBLE, temperature_max DOUBLE);
+CREATE TABLE weatherdata(
+    id IDENTITY PRIMARY KEY,
+    city VARCHAR(255),
+    date_measured DATE,
+    temperature_min DOUBLE,
+    temperature_max DOUBLE
+);
